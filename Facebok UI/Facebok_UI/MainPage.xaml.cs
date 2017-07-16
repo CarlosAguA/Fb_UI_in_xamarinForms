917f980723940296c69d6177a7cbe97f2bb87ee5
Carlos Aguilera <ceaguileraa@tecmor.mx>
2017-07-16 13:41:08 -0500

    initial commit

---
+++ Facebok UI/Facebok_UI/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Facebok_UI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+             // var Item1 = new ToolbarItem  
+           //{  
+           //    Text = "Item 1"  
+  
+           //};  
+           // var Item2 = new ToolbarItem  
+           // {  
+           //     Text = "Item 2",  
+           //     Order = ToolbarItemOrder.Secondary  
+  
+           // };  
+  
+           // this.ToolbarItems.Add(Item1);  
+           // this.ToolbarItems.Add(Item2);  
+        }
+    }
+}
